dee3b43164e3e63b4f8011255ee8da8f85f715f8
SungMin Yoon <sungmin.yoon@neuromeka.com>
2021-01-22 00:16:31 +0900

    first commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by SungMin Yoon on 2021/01/20.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
