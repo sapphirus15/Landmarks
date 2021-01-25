d5df23275cf8019e70ae383a2591db8cbc2fb446
SungMin Yoon <sapphirus15@gmail.com>
2021-01-26 00:42:45 +0900

    Section7. Pass Data into Child Views

--- Landmarks/Views/LandmarkDetail.swift
+++ Landmarks/Views/LandmarkDetail.swift
@@ -8,12 +8,14 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    var landmark: Landmark
+    
     var body: some View {
-        VStack {
-            MapView()
+        ScrollView {
+            MapView(coordinate: landmark.locationCoordinate)
                 .ignoresSafeArea(edges: .top)
                 .frame(height:300)
-            CircleImage()
+            CircleImage(image: landmark.image)
                 .offset(y:-130)
                 .padding(.bottom, -130.0)
             VStack(alignment: .leading) {
@@ -35,13 +37,15 @@ struct LandmarkDetail: View {
                 Text("Descriptive text goes here.")
             }
             .padding()
-            Spacer()
+            
         }
+        .navigationTitle(landmark.name)
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 struct LandmarkDetail_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkDetail()
+        LandmarkDetail(landmark: landmarks[0])
     }
 }
--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -11,9 +11,9 @@ struct LandmarkList: View {
     var body: some View {
         NavigationView{
             List(landmarks) { landmark in
-                NavigationLink(destination: LandmarkDetail()) {
-                    LandmarkRow(landmark: landmark)
-                }
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                                    LandmarkRow(landmark: landmark)
+                                }
             }
             .navigationTitle("Landmarks")
         }
