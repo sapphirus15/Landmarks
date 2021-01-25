079f498f31ac8fd8db9d9e92b146b984b72c4e37
SungMin Yoon <sapphirus15@gmail.com>
2021-01-26 00:50:46 +0900

    Generate Previews Dynamically

--- Landmarks/Views/LandmarkDetail.swift
+++ Landmarks/Views/LandmarkDetail.swift
@@ -9,40 +9,43 @@ import SwiftUI
 
 struct LandmarkDetail: View {
     var landmark: Landmark
-    
+
     var body: some View {
-        ScrollView {
+        VStack {
             MapView(coordinate: landmark.locationCoordinate)
                 .ignoresSafeArea(edges: .top)
-                .frame(height:300)
+                .frame(height: 300)
+
             CircleImage(image: landmark.image)
-                .offset(y:-130)
-                .padding(.bottom, -130.0)
+                .offset(y: -130)
+                .padding(.bottom, -130)
+
             VStack(alignment: .leading) {
-                Text("Turtle Rock")
+                Text(landmark.name)
                     .font(.title)
-                    .foregroundColor(.black)
+                    .foregroundColor(.primary)
+
                 HStack {
-                    Text("Joshua Tree National Park")
+                    Text(landmark.park)
                     Spacer()
-                    Text("Califonia")
+                    Text(landmark.state)
                 }
                 .font(.subheadline)
                 .foregroundColor(.secondary)
-                
+
                 Divider()
-                
-                Text("About Turtle Rock")
+
+                Text("About \(landmark.name)")
                     .font(.title2)
-                Text("Descriptive text goes here.")
+                Text(landmark.description)
             }
             .padding()
-            
         }
         .navigationTitle(landmark.name)
         .navigationBarTitleDisplayMode(.inline)
     }
 }
+
 
 struct LandmarkDetail_Previews: PreviewProvider {
     static var previews: some View {
