7c0ee44573b3460e7e23cf81216fabe901529d61
SungMin Yoon <sapphirus15@gmail.com>
2021-02-02 00:03:17 +0900

    Add a Category View

---
+++ Landmarks/Views/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by SungMin Yoon on 2021/02/02.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+            Text("Hello SwitfUI")
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
